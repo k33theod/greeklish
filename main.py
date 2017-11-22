a438304105d6ae644224fbc6d039419d73ec5a61
k33theod <k33theod@gmail.com>
2017-11-22 09:27:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import unicodedata
-def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
-
-
 
 greek_to_latin={'α': 'a', 'β': 'v', 'γ': 'g', 'δ': 'd', 'ε': 'e', 'ζ': 'z',
                 'η': 'h', 'θ': 'th', 'ι': 'i', 'κ': 'k', 'λ': 'l', 'μ': 'm',
